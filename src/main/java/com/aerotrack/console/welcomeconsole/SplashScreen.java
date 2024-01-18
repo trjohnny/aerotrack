74d726704c3faeff7005c406f6bbf0f9ba639ad7
Giovanni <jacket.gav@gmail.com>
2024-01-18 01:15:43 +0100

    fix: build

--- src/main/java/com/aerotrack/console/welcomeconsole/SplashScreen.java
+++ src/main/java/com/aerotrack/console/welcomeconsole/SplashScreen.java
@@ -1,13 +1,19 @@
 package com.aerotrack.console.welcomeconsole;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JWindow;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.net.URL;
 
 public class SplashScreen extends JWindow {
-    public SplashScreen(String imagePath) {
-        ImageIcon imageIcon = new ImageIcon(imagePath);
-        JLabel imageLabel = new JLabel(imageIcon);
-        imageLabel.setSize(200, 200);
+    public SplashScreen() {
+        URL imageUrl = getClass().getResource("/bg_logo.png");
+        if (imageUrl == null) {
+            throw new IllegalArgumentException("Resource not found");
+        }
+        JLabel imageLabel = new JLabel( new ImageIcon(imageUrl));
         getContentPane().add(imageLabel, BorderLayout.CENTER);
         setBackground(new Color(0, 0, 0, 0));
         pack();
